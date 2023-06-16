1d00780acda1314a4be4fd0845b8d10443174134
ashloverscn <ashloverscn@gmail.com>
2023-06-16 07:02:47 +0530

    Update part8.install-vicidial-crontab-from-file.sh

--- part8.install-vicidial-crontab-from-file.sh
+++ part8.install-vicidial-crontab-from-file.sh
@@ -1,4 +1,5 @@
-#Install crontab entry from file
+echo -e "\e[0;32m Install crontab entry from vici-cron \e[0m"
+sleep 2
 cd /usr/src
 \cp -r /usr/src/vici-cron /usr/src/vici-cron.original 
 wget -O /usr/src/vici-cron https://github.com/ashloverscn/Vicidial-Scratch-Install-CentOS-7-2207-2-x86_64-Minimal-Server/raw/main/vici-cron
