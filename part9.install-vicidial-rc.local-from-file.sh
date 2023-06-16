80c1c599875461ffac6115599f6c06b990bb36bc
ashloverscn <ashloverscn@gmail.com>
2023-06-16 07:36:50 +0530

    Update part9.install-vicidial-rc.local-from-file.sh

--- part9.install-vicidial-rc.local-from-file.sh
+++ part9.install-vicidial-rc.local-from-file.sh
@@ -5,3 +5,7 @@ cd /usr/src
 wget -O /usr/src/rc.local https://github.com/ashloverscn/Vicidial-Scratch-Install-CentOS-7-2207-2-x86_64-Minimal-Server/raw/main/rc.local
 \cp -r /usr/src/rc.local /etc/rc.d/rc.local
 
+chmod +x /etc/rc.d/rc.local
+systemctl enable rc-local
+systemctl start rc-local
+
