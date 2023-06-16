ddcf88eda968418a67c429440271d56427b741ed
ashloverscn <ashloverscn@gmail.com>
2023-06-16 05:54:08 +0530

    Create part6.a.compile-asterisk-menuselect.sh

---
+++ part6.a.compile-asterisk-menuselect.sh
@@ -0,0 +1,11 @@
+echo -e "\e[0;32m Install Asterisk \e[0m"
+sleep 2
+cd /usr/src
+#yum remove asterisk* -y
+yum install asterisk* -y
+wget http://download.vicidial.com/required-apps/asterisk-13.29.2-vici.tar.gz
+#wget http://download.vicidial.com/beta-apps/asterisk-16.17.0-vici.tar.gz
+tar -xvzf asterisk-1*
+cd asterisk-*
+./configure --libdir=/usr/lib64 --with-gsm=internal --enable-opus --enable-srtp --with-ssl --enable-asteriskssl --with-pjproject-bundled --with-jansson-bundled
+make menuselect
