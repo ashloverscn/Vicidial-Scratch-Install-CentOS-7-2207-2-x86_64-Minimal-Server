fe29d024279b3c3b694c503027bdf13b16dc9bf2
ashloverscn <ashloverscn@gmail.com>
2023-06-19 03:08:30 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+clear
+
+echo -e "\e[0;32m Vicidial-Scratch-Install-CentOS-7-2207-2-x86_64-Minimal-Server \e[0m"
+echo -e "\e[0;32m INSTALLATION WILL START IN 10 SECONDS \e[0m"
+
+for i in {10..01}
+do
+tput cup 10 $l
+echo -n "$i"
+sleep 1
+done
+echo
+
+sleep 2
+
