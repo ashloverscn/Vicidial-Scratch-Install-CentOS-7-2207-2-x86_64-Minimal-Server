5f1127a7a24ae61f5fc969689e616cca8e0c5026
ashloverscn <ashloverscn@gmail.com>
2023-06-19 05:19:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
+
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 RESET='\033[0m'
-sec=11
-tput civis
-echo -ne "${GREEN}"
+sec=10
 
-while [ $sec -ge 0 ]; do
-echo -ne "$(printf "%02d" $sec):$(printf "%02d" $sec):$(printf "%02d" $sec)\0
-let "sec=sec-1"
-sleep 1
+tput civis
+echo -e "${YELLOW}"
+
+while [ $sec -ge 1 ]; do
+	clear
+	let "sec=sec-1"
+	echo -ne "${GREEN}"
+	echo -e "Vicidial-Scratch-Install-CentOS-7-2207-2-x86_64-Minimal-Server"
+	echo -ne "${YELLOW}"
+	echo -e "$(printf "INSTALLATION WILL START IN") $(printf "%02d" $sec) $(printf "SECONDS")\033[0K\r"
+	sleep 1
 done
 
-#clear
+#echo -e "${RESET}"
+echo -e "${GREEN}"
+tput cnorm
+clear
 
-#echo -e "\e[0;32m Vicidial-Scratch-Install-CentOS-7-2207-2-x86_64-Minimal-Server \e[0m"
-echo -e "\e[0;32m INSTALLATION WILL START IN 10 SECONDS \e[0m"
-
-#for i in {10..01}
-#do
-#tput cup 10 $l
-#echo -n "$i"
-#sleep 1
-#done
-#echo
-
-#sleep 2
-
+#####
+sudo su
+cd /usr/src
+ip a
