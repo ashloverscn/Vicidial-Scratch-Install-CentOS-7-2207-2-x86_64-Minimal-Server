1923a828031a22671457feb8e277d8b2460b8a27
ashloverscn <ashloverscn@gmail.com>
2023-06-19 13:07:58 +0530

    Update part3.install-perl-cpan-cpanm-modules.sh

--- part3.install-perl-cpan-cpanm-modules.sh
+++ part3.install-perl-cpan-cpanm-modules.sh
@@ -66,3 +66,52 @@ cpanm Text::CSV_XS
 cpanm readline --force
 
 
+cpanm File::HomeDir
+cpanm File::Which
+cpanm CPAN::Meta::Requirements
+cpanm CPAN
+cpanm YAML
+cpanm MD5
+cpanm Digest::MD5
+cpanm Digest::SHA1
+cpanm readline
+cpanm Bundle::CPAN
+cpanm DBI
+cpanm DBD::mysql
+cpanm Net::Telnet
+cpanm Time::HiRes
+cpanm Net::Server
+cpanm Switch
+cpanm Mail::Sendmail
+cpanm Unicode::Map
+cpanm Jcode
+cpanm Spreadsheet::WriteExcel
+cpanm OLE::Storage_Lite
+cpanm Proc::ProcessTable
+cpanm IO::Scalar
+cpanm Spreadsheet::ParseExcel
+cpanm Curses
+cpanm Getopt::Long
+cpanm Net::Domain
+cpanm Term::ReadKey
+cpanm Term::ANSIColor
+cpanm Spreadsheet::XLSX
+cpanm Spreadsheet::Read
+cpanm LWP::UserAgent
+cpanm HTML::Entities
+cpanm HTML::Strip
+cpanm HTML::FormatText
+cpanm HTML::TreeBuilder
+cpanm Time::Local
+cpanm MIME::Decoder
+cpanm Mail::POP3Client
+cpanm Mail::IMAPClient
+cpanm Mail::Message
+cpanm IO::Socket::SSL
+cpanm MIME::Base64
+cpanm MIME::QuotedPrint
+cpanm Crypt::Eksblowfish::Bcrypt
+cpanm Crypt::RC4
+cpanm Text::CSV
+cpanm Text::CSV_XS
+
