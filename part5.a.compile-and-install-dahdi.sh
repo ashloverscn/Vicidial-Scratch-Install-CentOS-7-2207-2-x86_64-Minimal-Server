60579df79a20083c2ec89c5b43938d3b19e8d40d
ashloverscn <ashloverscn@gmail.com>
2023-06-24 23:59:09 +0530

    Update part5.a.compile-and-install-dahdi.sh

--- part5.a.compile-and-install-dahdi.sh
+++ part5.a.compile-and-install-dahdi.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-
-echo -e "\e[0;32m Install Dahdi Audio_CODEC Driver \e[0m"
+ver=3.2.0
+echo -e "\e[0;32m Install Dahdi Audio_CODEC Driver v$ver \e[0m"
 sleep 2
 cd /usr/src
+#rm -rf dahdi-linux-complete*
 #yum remove dahdi* -y
 yum install dahdi* -y
 yum install dahdi-tools* -y
-wget http://download.vicidial.com/beta-apps/dahdi-linux-complete-2.11.1.tar.gz
-#wget http://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-current.tar.gz
-tar -xvzf dahdi-linux-complete-*
-cd dahdi-linux-complete-*
+#wget http://download.vicidial.com/beta-apps/dahdi-linux-complete-2.11.1.tar.gz
+wget -O dahdi-linux-complete-$ver+$ver.tar.gz https://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-$ver%2B$ver.tar.gz
+tar -xvzf dahdi-linux-complete-$ver+$ver.tar.gz
+cd dahdi-linux-complete-$ver+$ver
 make all
 make install
 make config
@@ -17,4 +18,3 @@ modprobe dahdi
 modprobe dahdi_dummy
 dahdi_genconf -v
 dahdi_cfg -v
-
