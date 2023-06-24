6dfab8704fff30ff3a898da98dbc8a688632ada0
ashloverscn <ashloverscn@gmail.com>
2023-06-25 02:45:28 +0530

    Update part6.compile-asterisk-make-install-and-samples-menuconfig-automated.sh

--- part6.compile-asterisk-make-install-and-samples-menuconfig-automated.sh
+++ part6.compile-asterisk-make-install-and-samples-menuconfig-automated.sh
@@ -6,11 +6,11 @@ echo -e "\e[0;32m Install Asterisk v$ver$oem \e[0m"
 sleep 2
 cd /usr/src
 #rm -rf asterisk*
+#rpm -i --replacepkgs /var/cache/zypp/packages/openSUSE-Leap-15.3-ViciDial-Ast13/x86_64/asterisk-13.38.2-lp153.7.23.x86_64.rpm
 #yum remove asterisk -y
 #yum remove asterisk-* -y
 #zypper remove asterisk -y
 #zypper remove asterisk-* -y
-#rpm -i --replacepkgs /var/cache/zypp/packages/openSUSE-Leap-15.3-ViciDial-Ast13/x86_64/asterisk-13.38.2-lp153.7.23.x86_64.rpm
 yum install asterisk -y
 yum install asterisk-* -y
 zypper install asterisk -y
