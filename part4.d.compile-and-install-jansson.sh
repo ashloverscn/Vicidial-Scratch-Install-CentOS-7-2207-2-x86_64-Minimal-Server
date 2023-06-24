c31ac50a5b3f7f9be22e65e47cee868299524388
ashloverscn <ashloverscn@gmail.com>
2023-06-24 23:27:25 +0530

    Update part4.d.compile-and-install-jansson.sh

--- part4.d.compile-and-install-jansson.sh
+++ part4.d.compile-and-install-jansson.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-
-echo -e "\e[0;32m Install Jansson \e[0m"
+ver=2.5
+echo -e "\e[0;32m Install Jansson v$ver \e[0m"
 sleep 2
 cd /usr/src/
 wget http://www.digip.org/jansson/releases/jansson-2.5.tar.gz
-tar -zxf jansson-2.5.tar.gz
-#tar xvzf jasson*
-cd jansson*
+tar -zxf jansson-$ver.tar.gz
+#tar xvzf jasson-$ver.tar.gz
+cd jansson-$ver
 ./configure
 make clean
 make
 make install
 ldconfig
-
