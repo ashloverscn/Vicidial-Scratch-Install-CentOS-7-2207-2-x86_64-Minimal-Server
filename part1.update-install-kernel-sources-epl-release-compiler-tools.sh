6b931d7df609e2db81ce5637636ef4ba3114c0af
ashloverscn <ashloverscn@gmail.com>
2023-06-28 23:36:38 +0530

    Update part1.update-install-kernel-sources-epl-release-compiler-tools.sh

--- part1.update-install-kernel-sources-epl-release-compiler-tools.sh
+++ part1.update-install-kernel-sources-epl-release-compiler-tools.sh
@@ -17,5 +17,9 @@ echo -e "\e[0;32m Set TimeZone Asia/Kolkata \e[0m"
 sleep 2
 timedatectl set-timezone Asia/Kolkata
 
+echo -e "\e[0;32m System updated with new kernel and must be REBOOTED \e[0m"
+echo -e "\e[0;32m Please Comment out #wget line and #./part1.*.sh in install.sh and rerun it after this boot\e[0m"
+echo -e "\e[0;32m System will REBOOT in 50 Seconds \e[0m"
+sleep 50 
 reboot
 
