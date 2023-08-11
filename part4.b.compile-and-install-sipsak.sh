05139f2842e5b712b91de5b1d112b050f02694b3
ashloverscn <ashloverscn@gmail.com>
2023-08-11 12:17:03 +0530

    Update part4.b.compile-and-install-sipsak.sh

--- part4.b.compile-and-install-sipsak.sh
+++ part4.b.compile-and-install-sipsak.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#https://568588.bugs.gentoo.org/attachment.cgi?id=419546
+#https://bugs.gentoo.org/attachment.cgi?id=419546&action=diff
+#https://packages.gentoo.org/packages/net-misc/sipsak/qa-report
+#https://github.com/nils-ohlmeier/sipsak
+#https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/sipsak.rb
+
 echo -e "\e[0;32m Install SIPSack \e[0m"
 sleep 2
 cd /usr/src
