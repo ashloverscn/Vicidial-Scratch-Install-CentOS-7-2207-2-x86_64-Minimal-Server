f0c59f2af97ed1b1b0275ff4104ce6f0a21c62e5
ashloverscn <ashloverscn@gmail.com>
2023-08-12 09:56:15 +0530

    Update part1.update-install-kernel-sources-epl-release-compiler-tools.sh

--- part1.update-install-kernel-sources-epl-release-compiler-tools.sh
+++ part1.update-install-kernel-sources-epl-release-compiler-tools.sh
@@ -13,10 +13,6 @@ echo -e "\e[0;32m Disable SeLinux \e[0m"
 sleep 2
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 
-echo -e "\e[0;32m Set TimeZone Asia/Kolkata \e[0m"
-sleep 2
-#timedatectl set-timezone Asia/Kolkata
-
 echo -e "\e[0;32m System updated with new kernel and must be REBOOTED \e[0m"
 echo -e "\e[0;32m Please Comment out #wget line and #./part1.*.sh in install.sh and rerun it after this boot\e[0m"
 echo -e "\e[0;32m System will REBOOT in 50 Seconds \e[0m"
