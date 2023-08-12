2b6b690da51eb73a6c71e3b28cee54958ba33b24
ashloverscn <ashloverscn@gmail.com>
2023-08-12 12:00:17 +0530

    Update part4.e.compile-and-install-eaccelerator.sh

--- part4.e.compile-and-install-eaccelerator.sh
+++ part4.e.compile-and-install-eaccelerator.sh
@@ -4,7 +4,7 @@ echo -e "\e[0;32m Install Eaccelerator - Maybe not nescessasy \e[0m"
 sleep 2
 cd /usr/src
 wget https://github.com/eaccelerator/eaccelerator/zipball/master -O eaccelerator.zip
-yum -y install
+yum -y install unzip
 unzip eaccelerator.zip
 cd eaccelerator-*
 export PHP_PREFIX="/usr"
