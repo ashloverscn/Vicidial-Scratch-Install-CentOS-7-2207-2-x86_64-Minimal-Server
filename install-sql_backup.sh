f457a4aad8ab9ec9c5a447d840b19ec7a349d91e
ashloverscn <ashloverscn@gmail.com>
2023-08-14 02:26:01 +0530

    Update install-sql_backup.sh

--- install-sql_backup.sh
+++ install-sql_backup.sh
@@ -15,16 +15,11 @@ mysql -uroot -e "create database asterisk"
 mysql -uroot asterisk < /usr/src/complete_backup_45.142.112.120.sql
 mysql -u root -f asterisk < /usr/src/astguiclient/trunk/extras/upgrade_2.x.sql
 
-#################### correct asterisk version #####################
-# confirm running asterisk version also same as in database
-# if not replace with the running asterisk's version 
-
+#correct asterisk version
 # to check running asterisk version 
 asterisk -rx "core show version"
-
 # to check database asterisk version 
 mysql -uroot -e "use asterisk ; select asterisk_version from servers"
-
 # to update database entry of asterisk version
 mysql -uroot -e "use asterisk ; update servers set asterisk_version='xx.xx.x-vici'"
 
