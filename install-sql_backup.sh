61d4f1c947eeecdfe21c0d77fd1d2b2b3cdd7afb
ashloverscn <ashloverscn@gmail.com>
2023-08-14 02:44:04 +0530

    Update install-sql_backup.sh

--- install-sql_backup.sh
+++ install-sql_backup.sh
@@ -1,7 +1,7 @@
 #to backup asterisk database into sql file 
-#mysqldump asterisk > /usr/src/complete_backup_45.142.112.120.sql
+#mysqldump asterisk > /usr/src/complete_backup_vicibox11_45.142.112.124.sql
 #to transfer backup to a new server
-#\scp -r /usr/src/complete_backup_45.142.112.120.sql root@45.142.112.126:/usr/src/
+#\scp -r /usr/src/complete_backup_vicibox11_45.142.112.124.sql root@45.142.112.126:/usr/src/
 
 #input server ip to ipvariable
 
@@ -9,11 +9,11 @@ cd /usr/src
 #sed $ipvariable to ip > backup.sql
 
 #sed $hostvariable to host-domain > backup.sql
-sed -i 's/45.142.112.120/45.142.112.126/g' /usr/src/complete_backup_45.142.112.120.sql
+sed -i 's/45.142.112.124/45.142.112.126/g' /usr/src/complete_backup_vicibox11_45.142.112.124.sql
 
 mysql -uroot -e "drop database asterisk"
 mysql -uroot -e "create database asterisk"
-mysql -uroot asterisk < /usr/src/complete_backup_45.142.112.120.sql
+mysql -uroot asterisk < /usr/src/complete_backup_vicibox11_45.142.112.124.sql
 mysql -u root -f asterisk < /usr/src/astguiclient/trunk/extras/upgrade_2.x.sql
 
 #correct asterisk version
