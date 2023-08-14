25a0f19e9654434883250024a09c4b4bc22c5b54
ashloverscn <ashloverscn@gmail.com>
2023-08-14 22:04:56 +0530

    Update part1.update-install-kernel-sources-epl-release-compiler-tools.sh

--- part1.update-install-kernel-sources-epl-release-compiler-tools.sh
+++ part1.update-install-kernel-sources-epl-release-compiler-tools.sh
@@ -14,7 +14,7 @@ sleep 2
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 
 echo -e "\e[0;32m System updated with new kernel and must be REBOOTED \e[0m"
-echo -e "\e[0;32m Please Comment out #wget line and #./part1.*.sh in install.sh and rerun it after this boot\e[0m"
+echo -e "\e[0;32m Please run /usr/src/./install.sh again after this boot\e[0m"
 echo -e "\e[0;32m System will REBOOT in 50 Seconds \e[0m"
 sleep 50 
 reboot
