e005c5c9c6ec765c73e875ad3d9fefcf2b0b3812
ashloverscn <ashloverscn@gmail.com>
2023-08-15 05:20:56 +0530

    Update install-sql_backup.sh

--- install-sql_backup.sh
+++ install-sql_backup.sh
@@ -17,7 +17,7 @@ echo "This is the OLD Server ip Address CONFIRM"$serveripadd
 echo "This Server ip Address will now be "$serveripadd
 echo "This Server Domain Name will now be "$domainname
 echo "Target backup file is /usr/src/"$backupfilename
-sleep 2
+sleep 5
 
 sed -i "s|${oldserveripadd}|${serveripadd}|g" /usr/src/$backupfilename
 sed -i "s|olddomainname|${domainname}|g" /usr/src/$backupfilename
@@ -35,6 +35,7 @@ read userphonepass
 
 echo "This Server New Admin User 6666 password will now be "$admin6666pass
 echo "This Server New User and Phone password will now be "$userphonepass
+sleep 5
 
 mysql -uroot -e "use asterisk ; select asterisk_version from servers"
 mysql -uroot -e "use asterisk ; update servers set asterisk_version='16.30.0'"
@@ -56,6 +57,7 @@ mysql -uroot -e "use asterisk ; select login,pass,conf_secret from phones"
 
 echo -e "\e[0;32m All Admin6666 and User-Phones Password Updated Rebooting System in 10 seconds \e[0m"
 sleep 10 
+
 reboot 
 
 #to backup asterisk database into sql file 
