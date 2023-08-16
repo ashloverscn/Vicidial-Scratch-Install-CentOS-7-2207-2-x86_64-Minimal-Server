7502706251f183d821e6320b8399441d523a03f4
ashloverscn <ashloverscn@gmail.com>
2023-08-17 05:11:43 +0530

    Update debloat.sh

--- debloat.sh
+++ debloat.sh
@@ -5,8 +5,10 @@ rm -rf ./firewalld
 rm -rf ./usr-lib64-asterisk-modules
 rm -rf ./asterisk*
 rm -rf ./dahdi*
-rm -rf ./libpri*
-rm -rf ./lame*
 rm -rf ./sipsak*
+rm -rf ./lame*
 rm -rf ./jansson*
 rm -rf ./eaccelerator*
+rm -rf ./libpri*
+
+
