b84c37878c8734a6a8fd94db64593b30ef5f1b93
ashloverscn <ashloverscn@gmail.com>
2023-08-17 05:08:46 +0530

    Create debloat.sh

---
+++ debloat.sh
@@ -0,0 +1,12 @@
+cd /usr/src/
+yum clean all
+rm -f ./*
+rm -rf ./firewalld
+rm -rf ./usr-lib64-asterisk-modules
+rm -rf ./asterisk*
+rm -rf ./dahdi*
+rm -rf ./libpri*
+rm -rf ./lame*
+rm -rf ./sipsak*
+rm -rf ./jansson*
+rm -rf ./eaccelerator*
