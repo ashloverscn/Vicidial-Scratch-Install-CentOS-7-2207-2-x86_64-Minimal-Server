8e36da32a45948f20f1757143b4286fb8db47896
ashloverscn <ashloverscn@gmail.com>
2023-08-17 01:35:29 +0530

    Create enable-PrintMotd.sh

---
+++ enable-PrintMotd.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mv /etc/motd /etc/motd.bak
+\cp -r /usr/src/motd /etc/motd
+
+cat /etc/ssh/sshd_config | grep "#PrintMotd yes"
+cat /etc/ssh/sshd_config | grep "PrintMotd yes"
+
+sed -i "s|#PrintMotd yes|PrintMotd yes|g" /etc/ssh/sshd_config
+#sed -i "s|PrintMotd yes|#PrintMotd yes|g" /etc/ssh/sshd_config
+
+
