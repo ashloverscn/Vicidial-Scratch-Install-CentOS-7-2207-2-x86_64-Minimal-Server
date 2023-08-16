0f32f81919f31b5969c314cbc1f93181028e6cbb
ashloverscn <ashloverscn@gmail.com>
2023-08-17 01:43:36 +0530

    Update enable-PrintMotd.sh

--- enable-PrintMotd.sh
+++ enable-PrintMotd.sh
@@ -9,4 +9,7 @@ cat /etc/ssh/sshd_config | grep "PrintMotd yes"
 sed -i "s|#PrintMotd yes|PrintMotd yes|g" /etc/ssh/sshd_config
 #sed -i "s|PrintMotd yes|#PrintMotd yes|g" /etc/ssh/sshd_config
 
+rm -rf ~/.hushlogin
+#touch ~/.hushlogin
+
 
