007c3fad3f29328284f66f07526f9468b582aca3
ashloverscn <ashloverscn@gmail.com>
2023-08-21 16:59:33 +0530

    Update install-fail2ban.sh

--- install-fail2ban.sh
+++ install-fail2ban.sh
@@ -12,9 +12,9 @@ systemctl start fail2ban
 
 yum -y install expect
 
-#echo -e "\e[0;32m Please Enter This Server IP ADDRESS to avoid self ban \e[0m"
-#sleep 2
-#read serveripadd
+echo -e "\e[0;32m Please Enter This Server IP ADDRESS to avoid self ban \e[0m"
+sleep 2
+read serveripadd
 
 echo "fail2ban self ignore ip will be set to "$serveripadd
 sleep 5
