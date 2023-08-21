bca3143f364f42695a4fe5c70a232b808241b04d
ashloverscn <ashloverscn@gmail.com>
2023-08-21 17:07:20 +0530

    Update part2.install-and-configure-php-mariadb-apache.sh

--- part2.install-and-configure-php-mariadb-apache.sh
+++ part2.install-and-configure-php-mariadb-apache.sh
@@ -117,7 +117,6 @@ echo "" > /etc/httpd/conf.d/0000-default-ssl.conf
 #\cp -r /usr/src/0000-default-ssl.conf /etc/httpd/conf.d/0000-default-ssl.conf
 mv /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.conf.bak
 
-#################################################################################
 echo -e "\e[0;32m Please Enter Redirect permanent https address \e[0m"
 sleep 2
 read serveripadd
