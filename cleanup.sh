57e5fd0f018f26f9e2945a7ae536d0b17c763dda
ashloverscn <ashloverscn@gmail.com>
2023-08-22 03:33:42 +0530

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,21 @@
+df -h
+sleep 5
+
+cd /usr/src/
+yum clean all
+rm -f ./*
+rm -rf ./firewalld
+rm -rf ./usr-lib64-asterisk-modules
+rm -rf ./etc-asterisk
+rm -rf ./viciphone-etc-asterisk
+rm -rf ./asterisk*
+rm -rf ./dahdi*
+rm -rf ./sipsak*
+rm -rf ./lame*
+rm -rf ./jansson*
+rm -rf ./eaccelerator*
+rm -rf ./libpri*
+
+df -h
+sleep 5
+
