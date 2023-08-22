959e4fe6289b9d1d0d7aef9d42c22782878c75fe
ashloverscn <ashloverscn@gmail.com>
2023-08-22 11:41:00 +0530

    Update part5.b.compile-and-install-libpri.sh

--- part5.b.compile-and-install-libpri.sh
+++ part5.b.compile-and-install-libpri.sh
@@ -5,8 +5,8 @@ echo -e "\e[0;32m Install LibPri v$ver\e[0m"
 sleep 2
 cd /usr/src
 #http://download.vicidial.com/required-apps/libpri-1.4.10.1.tar.gz
-#http://downloads.asterisk.org/pub/telephony/libpri/libpri-1.6.0.tar.gz
-wget http://downloads.asterisk.org/pub/telephony/libpri/libpri-$ver.tar.gz
+#http://downloads.asterisk.org/pub/telephony/libpri/releases/libpri-1.6.0.tar.gz
+wget http://downloads.asterisk.org/pub/telephony/libpri/releases/libpri-$ver.tar.gz
 tar -xvzf libpri-$ver.tar.gz
 cd libpri-$ver
 make
