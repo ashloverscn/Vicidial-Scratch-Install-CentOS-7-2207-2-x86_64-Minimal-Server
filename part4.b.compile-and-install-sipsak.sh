aefa64e3075792a5f5b4c60b2f7cb0899e87017a
ashloverscn <ashloverscn@gmail.com>
2023-08-22 12:49:49 +0530

    Update part4.b.compile-and-install-sipsak.sh

--- part4.b.compile-and-install-sipsak.sh
+++ part4.b.compile-and-install-sipsak.sh
@@ -4,7 +4,7 @@
 #https://bugs.gentoo.org/attachment.cgi?id=419546&action=diff
 #https://packages.gentoo.org/packages/net-misc/sipsak/qa-report
 #https://github.com/nils-ohlmeier/sipsak
-https://github.com/sangoma/sipsak
+#https://github.com/sangoma/sipsak
 #https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/sipsak.rb
 #http://netbsd.ftp.fu-berlin.de/pub/NetBSD/pkgsrc/packages/NetBSD/aarch64/9.3/All/sipsak-0.9.8.1.tgz
 #https://src.fedoraproject.org/repo/pkgs/sipsak/sipsak-0.9.6-1.tar.gz/c4eb8e282902e75f4f040f09ea9d99d5/sipsak-0.9.6-1.tar.gz
