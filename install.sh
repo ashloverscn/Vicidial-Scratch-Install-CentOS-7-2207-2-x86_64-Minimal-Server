a18e05281111ab6c2bb58337a17228f17d6f1af9
ashloverscn <ashloverscn@gmail.com>
2025-12-01 18:16:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,6 +28,40 @@ history -c
 
 clear
 
+#################################################################################
+echo "➜ Removing old repo files..."
+rm -f /etc/yum.repos.d/*.repo
+
+echo "➜ Creating new CentOS-Base.repo..."
+
+tee /etc/yum.repos.d/CentOS-Base.repo > /dev/null << 'EOF'
+[base]
+name=CentOS-7 - Base
+baseurl=https://mirrors.aliyun.com/centos-vault/7.9.2009/os/\$basearch/
+enabled=1
+gpgcheck=0
+
+[updates]
+name=CentOS-7 - Updates
+baseurl=https://mirrors.aliyun.com/centos-vault/7.9.2009/updates/\$basearch/
+enabled=1
+gpgcheck=0
+
+[extras]
+name=CentOS-7 - Extras
+baseurl=https://mirrors.aliyun.com/centos-vault/7.9.2009/extras/\$basearch/
+enabled=1
+gpgcheck=0
+EOF
+
+echo "➜ Cleaning YUM cache..."
+yum clean all
+
+echo "➜ Rebuilding YUM metadata..."
+yum makecache
+
+echo "✔ Repo Fix Complete!"
+echo "Run: yum repolist"
 
 #################################################################################
 cd /usr/src
