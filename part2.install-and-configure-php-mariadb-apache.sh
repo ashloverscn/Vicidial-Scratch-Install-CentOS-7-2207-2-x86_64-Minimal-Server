2c222dc3ea4acc52ec5ecd350afab044450a2485
ashloverscn <ashloverscn@gmail.com>
2026-04-24 19:31:29 +0530

    Update part2.install-and-configure-php-mariadb-apache.sh

--- part2.install-and-configure-php-mariadb-apache.sh
+++ part2.install-and-configure-php-mariadb-apache.sh
@@ -9,13 +9,17 @@ sleep 2
 
 #yum -y remove yum-utils
 
-yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum install epel-release -y
+
+#yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 
 yum -y install yum-utils
 
 yum-config-manager --enable remi-php74
+
+yum info php
 
 echo -e "\e[0;32m Configure fail2ban for vicidial with jail.local file \e[0m"
 sleep 2
