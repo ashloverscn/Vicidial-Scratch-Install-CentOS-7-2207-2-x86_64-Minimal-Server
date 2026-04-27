16a2199f05d0bcf77e65566f59583fade2d833a5
ashloverscn <ashloverscn@gmail.com>
2026-04-27 21:25:15 +0530

    Update part5.a.compile-and-install-dahdi.sh

--- part5.a.compile-and-install-dahdi.sh
+++ part5.a.compile-and-install-dahdi.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 ver=3.4.0
-#oem=1
-#ver=3.2.0
-oem=0
 
 echo -e "\e[0;32m Install Dahdi Audio_CODEC Driver v$ver \e[0m"
 sleep 2
@@ -15,20 +12,10 @@ yum install dahdi* -y
 yum install dahdi-tools* -y
 yum install gcc gcc-c++ make perl patch libedit-devel libuuid-devel libxml2-devel newt-devel -y
 
-if [ $oem -eq 1 ]
-then
-	wget http://download.vicidial.com/required-apps/dahdi-linux-complete-2.3.0.1+2.3.0.tar.gz
-	tar -xvzf dahdi-linux-complete-2.3.0.1+2.3.0.tar.gz
-	cd dahdi-linux-complete-2.3.0.1+2.3.0
-else
-	wget -O dahdi-linux-complete-$ver+$ver.tar.gz https://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-$ver+$ver.tar.gz
-	tar -xvzf dahdi-linux-complete-$ver+$ver.tar.gz
-	cd dahdi-linux-complete-$ver+$ver
-	sed -i 's/#if __has_attribute(__fallthrough__)/#if 0 \/* __has_attribute(__fallthrough__) *\//g' linux/include/dahdi/kernel.h
-fi
-
-#sed -i 's/netif_napi_add(netdev, &wc->napi, &wctc4xxp_poll, 64);/netif_napi_add(netdev, &wc->napi, wctc4xxp_poll);/g' /usr/src/dahdi-linux-complete-3.2.0+3.2.0/linux/drivers/dahdi/wctc4xxp/base.c
-#sudo sed -i 's|<linux/pci-aspm.h>|<linux/pci.h>|g' /usr/src/dahdi-linux-complete-3.2.0+3.2.0/linux/include/dahdi/kernel.h
+wget -O dahdi-linux-complete-$ver+$ver.tar.gz https://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-$ver+$ver.tar.gz
+tar -xvzf dahdi-linux-complete-$ver+$ver.tar.gz
+cd dahdi-linux-complete-$ver+$ver
+sed -i 's/#if __has_attribute(__fallthrough__)/#if 0 \/* __has_attribute(__fallthrough__) *\//g' linux/include/dahdi/kernel.h
 
 make all
 make install
