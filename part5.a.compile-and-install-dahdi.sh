0ff7e7057e620b1be3b33ffea8a5c806b62de9f8
ashloverscn <ashloverscn@gmail.com>
2026-04-27 21:29:53 +0530

    Update part5.a.compile-and-install-dahdi.sh

--- part5.a.compile-and-install-dahdi.sh
+++ part5.a.compile-and-install-dahdi.sh
@@ -36,7 +36,47 @@ make install-config
 cd /etc/dahdi
 \cp -r system.conf system.conf.bak
 \cp -r system.conf.sample system.conf
-systemctl enable dahdi
-systemctl start dahdi
 
+echo -e "\e[0;32m Enable dahdi.service in systemctl \e[0m"
+sleep 2
+
+\cp -r /etc/systemd/system/dahdi.service /etc/systemd/system/dahdi.service.bak
+rm -rf /etc/systemd/system/dahdi.service
+touch /etc/systemd/system/dahdi.service
+
+cat <<DAHDI>> /etc/systemd/system/dahdi.service
+
+[Unit]
+Description=DAHDI Telephony Drivers
+After=network.target
+Before=asterisk.service
+
+[Service]
+Type=oneshot
+ExecStartPre=/sbin/modprobe dahdi
+ExecStartPre=/sbin/modprobe dahdi_dummy
+ExecStart=/usr/sbin/dahdi_cfg -v
+ExecReload=/usr/sbin/dahdi_cfg -v
+ExecStop=/usr/sbin/dahdi_cfg -v
+Restart=on-failure
+RestartSec=2
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+
+DAHDI
+
+#restart dahdi Service
+systemctl daemon-reload && \
+systemctl disable dahdi.service && \
+systemctl enable dahdi.service && \
+systemctl restart dahdi.service && \
+systemctl status dahdi.service | head -n 18
+
+\cp -r /dahdi.sh /dahdi.sh.bak
+rm -rf /dahdi.sh
+\cp -r  /usr/src/dahdi.sh /dahdi.sh
+
+chmod +x /dahdi.sh 
 
