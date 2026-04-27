2dfff1324ecb0c1d12d09e12416e3746f8eea3d2
ashloverscn <ashloverscn@gmail.com>
2026-04-27 16:34:59 +0530

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -29,47 +29,6 @@ history -c
 clear
 
 #################################################################################
-echo "➜ Removing old repo files..."
-rm -f /etc/yum.repos.d/*.repo
-
-echo "➜ Creating new CentOS-Base.repo..."
-
-sudo tee /etc/yum.repos.d/CentOS-Base.repo > /dev/null << 'EOF'
-[base]
-name=CentOS-7 - Base
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/7.9.2009/os/$basearch/
-enabled=1
-gpgcheck=0
-
-[updates]
-name=CentOS-7 - Updates
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/7.9.2009/updates/$basearch/
-enabled=1
-gpgcheck=0
-
-[extras]
-name=CentOS-7 - Extras
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/7.9.2009/extras/$basearch/
-enabled=1
-gpgcheck=0
-
-[centosplus]
-name=CentOS-7 - Plus
-baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/7.9.2009/centosplus/$basearch/
-enabled=1
-gpgcheck=0
-EOF
-
-echo "➜ Cleaning YUM cache..."
-yum clean all
-
-echo "➜ Rebuilding YUM metadata..."
-yum makecache
-
-echo "✔ Repo Fix Complete!"
-echo "Run: yum repolist"
-
-#################################################################################
 cd /usr/src
 
 yum -y check-update
