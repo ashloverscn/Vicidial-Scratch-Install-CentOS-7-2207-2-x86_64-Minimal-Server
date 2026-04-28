06405591f6df885acaf0c5bf737e0581a9e34ae7
ashloverscn <ashloverscn@gmail.com>
2026-04-28 17:39:44 +0530

    Update part3.install-perl-cpan-cpanm-modules.sh

--- part3.install-perl-cpan-cpanm-modules.sh
+++ part3.install-perl-cpan-cpanm-modules.sh
@@ -2,139 +2,62 @@
 
 echo -e "\e[0;32m Install and Configure Perl-CPAN\Perl-CPAN-Modules \e[0m"
 sleep 2
-yum -y install perl-CPAN perl-YAML perl-libwww-perl perl-DBI perl-DBD-MySQL perl-GD perl-Env perl-Term-ReadLine-Gnu perl-SelfLoader perl-open.noarch
+cd /usr/src
+yum install -y perl-CPAN perl-YAML perl-CPAN-DistnameInfo perl-libwww-perl perl-DBI perl-DBD-MySQL perl-GD perl-Env perl-Term-ReadLine-Gnu perl-SelfLoader perl-open.noarch 
+##CPM install (Symlnk user cpm path to root bin)
+#curl -fsSL https://raw.githubusercontent.com/skaji/cpm/master/cpm > /usr/local/bin/cpm
+#curl -fsSL https://raw.githubusercontent.com/skaji/cpm/master/cpm > /bin/cpm
+#chmod +x /usr/local/bin/cpm
+#chmod +x /bin/cpm
+##setup cpm mirror (may be installed without mirrorS too)
+#export PERL_CPANM_OPT="--mirror http://www.cpan.org/ --mirror-only"
+##install cpm basic essential for all users globlly
+#cpm install -g JSON::PP
+#cpm install -g JSON::XS
+#cpm install -g App::cpanminus
+#cpm install -g App::cpm
+#cpm install -g JSON::PP JSON::XS App::cpanminus App::cpm
+#cpm install -g
+#CPM install using mirror argument
+#cpm install -g --mirror http://www.cpan.org JSON::PP JSON::XS App::cpanminus App::cpm
+#cpm install -g --mirror http://www.cpan.org
+##oneliner for CPM install
+#curl -fsSL https://raw.githubusercontent.com/skaji/cpm/main/cpm | perl - install -g JSON::PP JSON::XS App::cpanminus App::cpm
+#/usr/local/bin/cpm install -g
+##oneliner for CPM install using mirror
+#curl -fsSL https://raw.githubusercontent.com/skaji/cpm/main/cpm | perl - install -g --mirror http://www.cpan.org JSON::PP JSON::XS App::cpanminus App::cpm
+#/usr/local/bin/cpm install -g --mirror http://www.cpan.org 
 
-#cpan o conf init
-#rm -rf /usr/share/perl5/CPAN/Config.pm
-#if [ -d ~/.cpan ]; then rm -fR ~/.cpan ; echo y | cpan > /dev/null 2>&1; fi
+#!/bin/bash
 
-perl -MCPAN -e 'my $c = "CPAN::HandleConfig"; $c->load(doit => 1, autoconfig => 1); $c->edit(prerequisites_policy => "follow"); $c->edit(build_requires_install_policy => "yes"); $c->commit' 
+# --- 1. SETUP ISOLATED ENVIRONMENT ---
+# Create the directory for the modern Perl build
+mkdir -p /usr/local/perl5
 
-cd /usr/bin/
-#curl -LOk http://xrl.us/cpanm
-#curl -LOk https://github.com/ashloverscn/Vicidial-Scratch-Install-CentOS-7-2207-2-x86_64-Minimal-Server/raw/main/cpanm
-\cp -r /usr/src/cpanm ./cpanm
-cpan App::cpanminus 
-#cpanm --installdeps .
-chmod +x cpanm
-cpanm -f File::HomeDir
-cpanm -f File::Which
-cpanm CPAN::Meta::Requirements
-cpanm -f CPAN
-cpanm Tk::TableMatrix
-cpanm RPM::Specfile
-cpanm YAML
-cpanm MD5
-cpanm String::CRC
-cpanm Digest::MD5
-cpanm Digest::SHA1
-#cpanm readline --force
-cpanm Bundle::CPAN
-cpanm DBI
-cpanm -f DBD::mysql
-cpanm Net::Address::IP::Local
-cpanm Net::Address::IPv4::Local
-cpanm Net::Telnet
-cpanm Time::HiRes
-cpanm Net::Server
-cpanm Switch
-cpanm Mail::Sendmail
-cpanm Unicode::Map
-cpanm Jcode
-cpanm Spreadsheet::WriteExcel
-cpanm OLE::Storage_Lite
-cpanm Proc::ProcessTable
-cpanm IO::Scalar
-cpanm Spreadsheet::ParseExcel
-cpanm Curses
-cpanm Getopt::Long
-cpanm Net::Domain
-cpanm Term::ReadKey
-cpanm Term::ANSIColor
-cpanm Term::ReadLine::Gnu
-cpanm Spreadsheet::XLSX
-cpanm Spreadsheet::Read
-cpanm Spreadsheet::ReadSXC
-cpanm LWP::UserAgent
-cpanm HTML::Entities
-cpanm HTML::Strip
-cpanm HTML::FormatText
-cpanm HTML::TreeBuilder
-cpanm Time::Local
-cpanm MIME::Decoder
-cpanm Mail::POP3Client
-cpanm Mail::IMAPClient
-cpanm Mail::Message
-cpanm IO::Socket::SSL
-cpanm MIME::Base64
-cpanm MIME::QuotedPrint
-cpanm Crypt::Eksblowfish::Bcrypt
-cpanm Crypt::RC4
-cpanm Text::CSV
-cpanm Text::CSV_XS
-cpanm -f readline
+# --- 2. REBUILD PERL (THE ENGINE) ---
+# Download perl-build and compile Perl 5.38.0 into the isolated folder
+# This ensures we don't touch the system Perl (v5.16.3)
+curl -L https://raw.githubusercontent.com/tokuhirom/perl-build/master/perl-build | perl - 5.38.0 /usr/local/perl5
 
-echo -e "\e[0;32m Verify all cpan-modules installed successfuly \e[0m"
-sleep 2
-cpanm File::HomeDir
-cpanm File::Which
-cpanm CPAN::Meta::Requirements
-cpanm CPAN
-cpanm Tk::TableMatrix
-cpanm RPM::Specfile
-cpanm YAML
-cpanm MD5
-cpanm String::CRC
-cpanm Digest::MD5
-cpanm Digest::SHA1
-#cpanm readline --force
-cpanm Bundle::CPAN
-cpanm DBI
-cpanm DBD::mysql
-cpanm Net::Address::IP::Local
-cpanm Net::Address::IPv4::Local
-cpanm Net::Telnet
-cpanm Time::HiRes
-cpanm Net::Server
-cpanm Switch
-cpanm Mail::Sendmail
-cpanm Unicode::Map
-cpanm Jcode
-cpanm Spreadsheet::WriteExcel
-cpanm OLE::Storage_Lite
-cpanm Proc::ProcessTable
-cpanm IO::Scalar
-cpanm Spreadsheet::ParseExcel
-cpanm Curses
-cpanm Getopt::Long
-cpanm Net::Domain
-cpanm Term::ReadKey
-cpanm Term::ANSIColor
-cpanm Term::ReadLine::Gnu
-cpanm Spreadsheet::XLSX
-cpanm Spreadsheet::Read
-cpanm Spreadsheet::ReadSXC
-cpanm LWP::UserAgent
-cpanm HTML::Entities
-cpanm HTML::Strip
-cpanm HTML::FormatText
-cpanm HTML::TreeBuilder
-cpanm Time::Local
-cpanm MIME::Decoder
-cpanm Mail::POP3Client
-cpanm Mail::IMAPClient
-cpanm Mail::Message
-cpanm IO::Socket::SSL
-cpanm MIME::Base64
-cpanm MIME::QuotedPrint
-cpanm Crypt::Eksblowfish::Bcrypt
-cpanm Crypt::RC4
-cpanm Text::CSV
-cpanm Text::CSV_XS
-cpanm readline
+# --- 3. ACTIVATE MODERN PERL ---
+# Point the current session to the new Perl binary
+export PATH=/usr/local/perl5/bin:$PATH
+
+# --- 4. BOOTSTRAP SKAJI MODULES ---
+# Use the modern Perl to install cpm and cpanminus
+curl -fsSL https://raw.githubusercontent.com/skaji/cpm/main/cpm | perl - install -g --mirror http://www.cpan.org JSON::PP JSON::XS App::cpanminus App::cpm
+
+# --- 5. CREATE GLOBAL SYMLINKS ---
+# Map the new tools to /usr/local/bin so they are globally accessible
+ln -sf /usr/local/perl5/bin/cpm /usr/local/bin/cpm
+ln -sf /usr/local/perl5/bin/cpanm /usr/local/bin/cpanm
+
+# Refresh the shell's command path hash
+hash -r
+
+# --- 6. INSTALL TARGET MODULES ---
+# Finally, use the isolated cpm to install your required modules
+/usr/local/bin/cpm install -g --mirror http://www.cpan.org JSON::PP JSON::XS
 
 echo -e "\e[0;32m All cpan-modules installed and verified successfuly \e[0m"
 sleep 2
-
-
-
